fd8a375dc4f7f2edd2430190e9d5325c8dae9c49
endroni <rubao.rcs@gmail.com>
2022-08-02 16:53:33 -0300

    for import time sleep range

---
+++ modulo3/for3.py
@@ -0,0 +1,2 @@
+for i in range(-10, 10, 5):
+    print("The value of i is currently", i)
---
+++ modulo3/max.py
@@ -0,0 +1,10 @@
+# Lendo os valores
+number1 = int(input("Digite um número: "))
+number2 = int(input("Digite outro número: "))
+number3 = int(input("Mais um valor: "))
+
+# Compara e encontra o maior
+largest_number = max(number1, number2, number3)
+
+# Exibe o maior valor
+print("O maior número é: ", largest_number)
