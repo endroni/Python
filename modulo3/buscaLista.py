28f9450f0d71f502cf28291472a301c196e252a5
endroni <rubao.rcs@gmail.com>
2022-08-04 16:06:43 -0300

    listas

---
+++ modulo3/buscaLista.py
@@ -0,0 +1,7 @@
+# Verifica se um valor está ou não dentro de uma lista
+
+my_list = [0, 3, 12, 8, 2]
+
+print(5 in my_list)
+print(5 not in my_list)
+print(12 in my_list)
---
+++ modulo3/reverse.py
@@ -0,0 +1,3 @@
+my_list = [8, 10, 6, 2, 4]
+my_list.reverse()
+print(my_list)
